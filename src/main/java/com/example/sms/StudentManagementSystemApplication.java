44dea5d07421a579d0b92ba65aaf9fce73711fbb
HaziqRidzwan <haziqridzwan@outlook.com>
2021-12-29 09:39:07 +0800

    Second push

--- src/main/java/com/example/sms/StudentManagementSystemApplication.java
+++ src/main/java/com/example/sms/StudentManagementSystemApplication.java
@@ -1,30 +1,14 @@
 package com.example.sms;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.sms.entity.Student;
-import com.example.sms.repository.StudentRepository;
-
 @SpringBootApplication
-public class StudentManagementSystemApplication implements CommandLineRunner{
+public class StudentManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
 	}
 
-	@Autowired
-	private StudentRepository studentRepository;
-	
-	@Override
-	public void run(String... args) throws Exception {
-		Student student1 = new Student("Haziq", "Ridzwan", "haziqridzwan@outlook.com");
-		studentRepository.save(student1);
-		
-		Student student2 = new Student("Ummu", "Hanisah", "ummuhanisah@outlook.com");
-		studentRepository.save(student2);
-	}
-
 }
+
